133f11756b59f42f2b515742d27586fc2f422485
Tibor Ignéczi <47614820+sidragosam@users.noreply.github.com>
2023-07-31 22:35:46 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -2,28 +2,123 @@ var foszforDiv = document.getElementById("foszfor-info");
 var naohDiv = document.getElementById("naoh-info");
 var sosavDiv = document.getElementById("sosav-info");
 var dietilDiv = document.getElementById("dietil-info");
+var efedrinDiv = document.getElementById("efedrin-info");
+var kensavDiv = document.getElementById("kensav-info");
+var hidrogenjodDiv = document.getElementById("hidrogenjod-info");
+var hajnalkaDiv = document.getElementById("hajnalkamag-info");
+var hawaiiDiv = document.getElementById("hawaiimag-info");
+var kapszulaDiv = document.getElementById("kapszula-info");
+var kokalevDiv = document.getElementById("kokelev-info");
+var natrikabDiv = document.getElementById("natriumkarb-info");
+var kerozinDiv = document.getElementById("kerozin-info");
+var kaliumpermDiv = document.getElementById("kaliumperm-info");
+var lenmagolajDiv = document.getElementById("lenmagolaj-info");
+var petroleumDiv = document.getElementById("petroleum-info");
+var metanolDiv = document.getElementById("metanol-info");
 
-	function showFoszforInfo() {
-      foszforDiv.style.display = "block";
-      naohDiv.style.display = "none";
-      sosavDiv.style.display = "none";
-      dietilDiv.style.display = "none";
-    }
-	function showNaohInfo() {
-      foszforDiv.style.display = "none";
-      naohDiv.style.display = "block";
-      sosavDiv.style.display = "none";
-      dietilDiv.style.display = "none";
-    }
-	function showSavInfo() {
-      foszforDiv.style.display = "none";
-      naohDiv.style.display = "none";
-      sosavDiv.style.display = "block";
-      dietilDiv.style.display = "none";
-    }
-	function showDietilInfo() {
-      foszforDiv.style.display = "none";
-      naohDiv.style.display = "none";
-      sosavDiv.style.display = "none";
-      dietilDiv.style.display = "block";
-    }
+function noneBlocks() {
+	foszforDiv.style.display = "none";
+    naohDiv.style.display = "none";
+    sosavDiv.style.display = "none";
+    dietilDiv.style.display = "none";
+	efedrinDiv.style.display = "none";
+	kensavDiv.style.display = "none";
+	hidrogenjodDiv.style.display = "none";
+	hajnalkaDiv.style.display = "none";
+	hawaiiDiv.style.display = "none";
+	kapszulaDiv.style.display = "none";
+	kokalevDiv.style.display = "none";
+	natrikabDiv.style.display = "none";
+	kerozinDiv.style.display = "none";
+	kaliumpermDiv.style.display = "none";
+	lenmagolajDiv.style.display = "none";
+	petroleumDiv.style.display = "none";
+	metanolDiv.style.display = "none";
+}
+
+function showMetanolInfo() {
+	noneBlocks();
+	metanolDiv.style.display = "block";
+}
+
+function showPetroleumInfo() {
+	noneBlocks();
+	petroleumDiv.style.display = "block";
+}
+
+function showLenmagolajInfo() {
+	noneBlocks();
+	lenmagolajDiv.style.display = "block";
+}
+
+function showKaliumpermInfo() {
+	noneBlocks();
+	kaliumpermDiv.style.display = "block";
+}
+
+function showKerozinInfo() {
+	noneBlocks();
+	kerozinDiv.style.display = "block";
+}
+
+function showNatrikabInfo() {
+	noneBlocks();
+	natrikabDiv.style.display = "block";
+}
+
+function showKokalevInfo() {
+	noneBlocks();
+	kokalevDiv.style.display = "block";
+}
+
+function showKapszulaInfo() {
+	noneBlocks();
+	kapszulaDiv.style.display = "block";
+}
+
+function showHawaiiInfo() {
+	noneBlocks();
+	hawaiiDiv.style.display = "block";
+}
+
+function showFoszforInfo() {
+	noneBlocks();
+    foszforDiv.style.display = "block";
+}
+
+function showNaohInfo() {
+	noneBlocks();
+    naohDiv.style.display = "block";
+}
+	
+function showSavInfo() {
+	noneBlocks();
+    sosavDiv.style.display = "block";
+}
+
+function showDietilInfo() {
+	noneBlocks();
+    dietilDiv.style.display = "block";
+}
+
+function showEfeInfo() {
+	noneBlocks();
+	efedrinDiv.style.display = "block";
+}
+
+function showKensavInfo() {
+	noneBlocks();
+	kensavDiv.style.display = "block";
+}
+
+function showHidrogenJodInfo() {
+	noneBlocks();
+	hidrogenjodDiv.style.display = "block";
+}
+
+function showHajnalkaInfo() {
+	noneBlocks();
+	hajnalkaDiv.style.display = "block";
+}
+
+
